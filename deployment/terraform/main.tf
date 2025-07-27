0782c34c11cdd64475eabae3b02bb85c70c0ae17
sgmselli <sellings.matthew@gmail.com>
2025-07-27 16:56:25 +0100

    Retrieve state from s3 bucket and lock with dynamodb

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-bucket-a7f3d9c2"
+    key            = "deployment/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = var.region
 }
