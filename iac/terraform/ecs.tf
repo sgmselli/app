95271dfdb5cd2df93279cc663e33e43b987f33c4
sgmselli <sellings.matthew@gmail.com>
2025-07-29 16:47:57 +0100

    Temporarily make fix for backend not starting by allowing public ip

--- iac/terraform/ecs.tf
+++ iac/terraform/ecs.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "backend_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [data.aws_vpc.default.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -206,7 +206,7 @@ resource "aws_ecs_service" "backend" {
 
   network_configuration {
     subnets          = data.aws_subnets.default.ids
-    assign_public_ip = false
+    assign_public_ip = true
     security_groups  = [aws_security_group.backend_sg.id]
   }
 
