69faba070bac17700951338a78d49f524e697a98
sgmselli <sellings.matthew@gmail.com>
2025-09-20 20:04:15 +0100

    Add allow origins to production and development. Overcome cors policy by setting api url to /api/v1

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { InternalAxiosRequestConfig, AxiosInstance, AxiosResponse, AxiosError } from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = "/api/v1";
 
 export const api: AxiosInstance = axios.create({
   baseURL: BASE_URL,
